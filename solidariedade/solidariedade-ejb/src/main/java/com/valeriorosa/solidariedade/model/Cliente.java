c605d4e6ce25cb344f7c6a586fc5d71e85f343a9
Tiago da Rosa Valério - Developer <tiago_valerio_betha@hotmail.com>
2017-09-22 20:25:08 -0300

    Exercício Serviços Rest
    Exercício Serviços Rest

---
+++ solidariedade/solidariedade-ejb/src/main/java/com/valeriorosa/solidariedade/model/Cliente.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.valeriorosa.solidariedade.model;
+
+/**
+ *
+ * @author tiagorv
+ */
+public class Cliente {
+    
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String celular;
+    private String cidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+}
